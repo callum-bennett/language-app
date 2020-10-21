124c28da64a36765f8805ee3048ca3e33fc186d2
Callum Bennett <callumbennett.cb@gmail.com>
2020-10-21 20:23:00 +0100

    Fix button styles, fix textInput

--- components/AppButton.js
+++ components/AppButton.js
@@ -21,11 +21,17 @@ const AppButton = (props) => {
       hitSlop={{ left: 20, right: 20, top: 20, bottom: 20 }}
       onPress={props.onPress}
     >
-      <View style={styles.button}>
+      <View style={[styles.button, props.style.button]}>
         <Text style={styles.text}>{props.children}</Text>
       </View>
     </Touchable>
   );
+};
+
+AppButton.defaultProps = {
+  style: {
+    button: {},
+  },
 };
 
 const styles = StyleSheet.create({
--- components/AppTextInput.js
+++ components/AppTextInput.js
@@ -1,13 +1,30 @@
 import React from "react";
-import { StyleSheet, TextInput } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
+import AppText from "./AppText";
 
 const AppTextInput = (props) => {
-  return <TextInput {...props} style={[styles.textInput, props.style]} />;
+  return (
+    <View style={styles.container}>
+      {props.label && <AppText style={styles.label}>{props.label}</AppText>}
+      <TextInput {...props} style={[styles.textInput, props.style]} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 14,
+  },
   textInput: {
     fontFamily: "roboto",
+    paddingVertical: 5,
+    borderBottomColor: "#DDD",
+    borderBottomWidth: 2,
+    flexGrow: 1,
   },
 });
 
