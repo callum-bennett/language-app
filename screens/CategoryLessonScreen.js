3fcb5fb9df7b0ecbfac5f8b439f105d70d48c8e8
Callum Bennett <callumbennett.cb@gmail.com>
2020-11-01 17:23:37 +0000

    Add lesson complete page

--- screens/CategoryLessonScreen.js
+++ screens/CategoryLessonScreen.js
@@ -4,19 +4,15 @@ import Lesson from "../components/Lesson";
 import { useSelector } from "react-redux";
 import { selectWordsByLessonId } from "../store/selectors/word";
 import Crossword from "../components/Lesson/Crossword";
+import AppButton from "../components/AppButton";
 import AppText from "../components/AppText";
 
 const CategoryLessonScreen = (props) => {
   const lessonId = props.route.params.lessonId;
   const words = useSelector((state) => selectWordsByLessonId(state, lessonId));
 
-  const [lessonStarted, setLessonStarted] = useState(false);
   const [lessonComplete, setLessonComplete] = useState(false);
   const [gameComplete, setGameComplete] = useState(false);
-
-  const handleStartLesson = () => {
-    setLessonStarted(true);
-  };
 
   const handleCompleteLesson = () => {
     setLessonComplete(true);
@@ -37,13 +33,20 @@ const CategoryLessonScreen = (props) => {
       ) : !gameComplete ? (
         <Crossword words={words} onComplete={handleCompleteGame} />
       ) : (
-        <AppText onPress={handleSectionComplete}>Section Complete!</AppText>
+        <>
+          <AppText style={styles.completeText}>Lesson complete!</AppText>
+          <AppButton onPress={handleSectionComplete}>Continue</AppButton>
+        </>
       )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  completeText: {
+    fontSize: 28,
+    marginBottom: 40,
+  },
   screen: {
     flex: 1,
     justifyContent: "center",
