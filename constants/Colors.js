daec1557433ee66818c9d5fe78a8c230c2d68c5d
Callum Bennett <callum.bennett@enovation.ie>
2020-11-08 21:23:49 +0000

    Add disabled button style

--- constants/Colors.js
+++ constants/Colors.js
@@ -2,7 +2,7 @@ export const primary = "#f45b69";
 export const accent = "#59c3c3";
 export const alt = "#4062bb";
 
-export const light = "#ebebeb";
+export const light = "#cbc9c9";
 export const dark = "#160c28";
 
 export const error = "#aa0000";
