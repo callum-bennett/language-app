337d07121b5fae40b8aa099d0d0d59721250300c
Callum Bennett <callumbennett.cb@gmail.com>
2020-11-11 20:23:02 +0000

    Fix error handling, display activity icon on sign in

--- api/client.js
+++ api/client.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-community/async-storage";
+import { availableRoutes, navigate } from "../navigation/RootNavigation";
 
 const apiClient = axios.create({
-  baseURL: "http://192.168.0.7:8000",
+  baseURL: "http://192.168.0.7:8001",
   timeout: 2000,
 });
 
@@ -19,10 +20,24 @@ apiClient.interceptors.response.use(
   (res) => res,
   (error) => {
     const { response } = error;
+    const redirectRoute = availableRoutes().contains("Authentication")
+      ? "Authentication"
+      : "Categories";
 
     if (!response) {
-    } else if (response.status === 401) {
-      AsyncStorage.removeItem("authToken");
+      return navigate(redirectRoute, {
+        error: "Oops! Something went wrong",
+      });
+    } else {
+      const { status } = response;
+      if (status === 401) {
+        AsyncStorage.removeItem("authToken");
+        return navigate("Authentication");
+      } else if (status === 500) {
+        return navigate(redirectRoute, {
+          error: "Oops! Something went wrong",
+        });
+      }
     }
 
     return Promise.reject(error);
