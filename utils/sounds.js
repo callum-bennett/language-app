5b2939aa271e2526b45f4a1a415e4db39b1ce3a2
Callum Bennett <callumbennett.cb@gmail.com>
2020-11-12 21:18:13 +0000

    use async await in playSound

--- utils/sounds.js
+++ utils/sounds.js
@@ -3,13 +3,12 @@ import { Audio } from "expo-av";
 export const FEEDBACK_POSITIVE = require("../assets/sounds/success.wav");
 export const FEEDBACK_NEGATIVE = require("../assets/sounds/error.wav");
 
-export const playSound = (sound) => {
-  Audio.Sound.createAsync(sound, { shouldPlay: true })
-    .then((res) => {
-      res.sound.setOnPlaybackStatusUpdate((status) => {
-        if (!status.didJustFinish) return;
-        res.sound.unloadAsync().catch(() => {});
-      });
-    })
-    .catch((error) => {});
+export const playSound = async (sound) => {
+  try {
+    const res = await Audio.Sound.createAsync(sound, { shouldPlay: true });
+    res.sound.setOnPlaybackStatusUpdate((status) => {
+      if (!status.didJustFinish) return;
+      res.sound.unloadAsync();
+    });
+  } catch (err) {}
 };
