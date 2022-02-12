74aa7d7a46b8107fdc781d2e00edbee896504a4f
Callum Bennett <callumbennett.cb@gmail.com>
2022-02-12 16:08:37 +0000

    Add missing utils exports

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,6 @@
+export * from "./crosswordGenerator";
+export * from "./sounds";
+
 export const arrayToObjectByKey = (array, key = "id") => {
   return array.reduce((object, arrayItem) => {
     object[arrayItem[key]] = arrayItem;
